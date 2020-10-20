dd6eb49ae46f8a2cb24fb5ccecfb3ebbe044ab89
harshcroods <harsh.croods@gmail.com>
2020-10-20 10:13:35 +0530

    first commit.

---
+++ SpringSecurityBasicAuth/src/main/java/com/croods/springsecurity/SpringSecurityBasicAuthApplication.java
@@ -0,0 +1,13 @@
+package com.croods.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityBasicAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityBasicAuthApplication.class, args);
+	}
+
+}
